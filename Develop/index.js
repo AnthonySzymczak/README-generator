98f9ca1be1aa9dddd8169094d7c6597624968188
AnthonySzymczak <doyalajs1@live.com>
2021-06-17 14:12:11 -0400

    setting up hypothetical generateMarkdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
 
+const fs = require('fs');
+const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of this project? : ',
+    'Please write a description of your project: ',
+    'What is the "user story" or usage of this project : ',
+    'Please list your installation instructions : ',
+    'Who contributed to this project? : ',
+    'Are there any specific tests you or your contributers have run? List them here: ',
+    'List the Test instructions here: ',
+    'Will this project include a license? : ',
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,68 @@
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+  /*  if 
+      ${andswers.license} = '' 
+    return`
+    ![badge](https://img.shields.io/badge/license-${answers.license}-yellow)
+    
+    `
+    else{
+    return `[]`  
+    
+     };
+  
+  */ 
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
 
+  
+};
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  <h1>${answers.title}</h1>
+  
+  ##Table of Contents :
+  -[License](#license)
+  -[Descption of application](#description)
+  -[Usage of application](#usage)
+  -[Packages needed for installation](#install)
+  -[Contributers of this application](#contributers)
+  -[Tests Ran](#allTest)
+
+  ## License
+  ${answers.license}
+  ## Description of application
+  ${answers.description}
+  ## Usage of application
+  ${answers.usage}
+  ## Packages needed for installation
+  ${answers.install}
+  ## Contributers of this application
+  ${answers.contributers}
+  ## Tests Ran
+  ${answers.allTest}
+
 
 `;
 }
+
+
+
+
+
 
 module.exports = generateMarkdown;
